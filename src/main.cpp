9510f7e07d3bbd15735eb28e388a24cc88951247
yorai <yorai@mit.edu>
2020-03-17 16:31:25 -0400

    Typo.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(){
     std::string patch_1_path = "media/1_20.png";
     cv::Mat patch1 = cv::imread(patch_1_path);
 
-    std::string patch_2_path = "media/2_20.png";
+    std::string patch_2_path = "media/1_21.png";
     cv::Mat patch2 = cv::imread(patch_2_path);
 
 
